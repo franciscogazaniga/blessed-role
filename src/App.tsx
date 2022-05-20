d07c4679e6ef86c4b28c442f43808084026f7b54
Francisco Eduardo <francisco.gazaniga@hotmail.com>
2022-05-19 21:42:44 -0300

    Created SearchBar, Recents and BoxContent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header";
+import { Recents } from "./components/Recents";
+import { SearchBar } from "./components/SearchBar";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
       <Header />
+      <SearchBar />
+      <Recents />
       <GlobalStyle />
     </>
   );     
